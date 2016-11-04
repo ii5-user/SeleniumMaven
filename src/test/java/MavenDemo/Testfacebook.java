e534268693a30c3a13382dd811ceeba9685fdbad
Sujit Soni <sujit.soni@ii5.local>
2016-11-04 16:25:39 +0530

    Third Commit

--- src/test/java/MavenDemo/Testfacebook.java
+++ src/test/java/MavenDemo/Testfacebook.java
@@ -5,16 +5,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Testfacebook {
-
+	WebDriver driver;
 	@Test
 	public void TestFireFox(){
 
 		System.setProperty("webdriver.firefox.marionette",".\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		 driver = new FirefoxDriver();
 		driver.get("http://www.facebook.com");
-		System.out.println("Applsssiccation titlgghe is y============="+driver.getTitle());
-		driver.quit();
+		System.out.println("Applsssiccaghththtion titlgghe is y============="+driver.getTitle());
 
+	}
+	@Test
+	public void Test2()
+	{
+		System.out.println("Test 2");
+		driver.quit();
 	}
 
 }
