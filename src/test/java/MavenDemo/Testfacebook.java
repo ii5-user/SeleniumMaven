0d971f1d421164533a022b4cf1ab6194f1243522
Sujit Soni <sujit.soni@ii5.local>
2016-11-04 17:12:35 +0530

    7 com

--- src/test/java/MavenDemo/Testfacebook.java
+++ src/test/java/MavenDemo/Testfacebook.java
@@ -1,7 +1,7 @@
 package MavenDemo;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Testfacebook {
@@ -9,17 +9,12 @@ public class Testfacebook {
 	@Test
 	public void TestFireFox(){
 
-		System.setProperty("webdriver.firefox.marionette",".\\geckodriver.exe");
-		 driver = new FirefoxDriver();
+		 driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
 		System.out.println("Applsssiccaghththtion titlgghe is y============="+driver.getTitle());
+		driver.quit();
 
 	}
-	@Test
-	public void Test2()
-	{
-		System.out.println("Test 2");
-		driver.quit();
-	}
+	
 
 }
